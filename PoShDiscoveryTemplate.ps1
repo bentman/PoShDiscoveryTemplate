e77945965d2bd5bdc91344879fe50c1ee2605362
bentman <26147320+bentman@users.noreply.github.com>
2023-07-03 16:21:13 -0500

    Update PoShDiscoveryTemplate.ps1

--- PoShDiscoveryTemplate.ps1
+++ PoShDiscoveryTemplate.ps1
@@ -1,44 +1,33 @@
 ﻿<#
 .SYNOPSIS
-PoShDiscoveryReport.ps1 - PowerShell Discovery Report Template
-
+    PoShDiscoveryReport.ps1 - PowerShell Discovery Report Template
 .DESCRIPTION
-The PoSh Discovery Report template automates the process of gathering information by generating standardized text reports in various environments. Create snippets with ChatGPT by using this template to streamline information gathering and produce repeatable reports.
-
+    The PoSh Discovery Report template automates the process of gathering information by generating standardized text reports in various environments.
+    Create snippets with ChatGPT by using this template to streamline information gathering and produce repeatable reports.
 .KEY FEATURES
-- Simplifies the generation of quick text reports on the logged-in user's desktop.
-- Use ChatGPT to customize information gathering snippets with tailored conditions.
-- Enhance error control and provide descriptive explanations in snippets.
-
+    - Simplifies the generation of quick text reports on the logged-in user's desktop.
+    - Use ChatGPT to customize information gathering snippets with tailored conditions.
+    - Enhance error control and provide descriptive explanations in snippets.
 .USAGE
-1. Copy the provided template from the repository.
-
-2. Utilize ChatGPT to interact with the template and store it for later use.
-   Example: GPT-Prompt> Store the following as a template to produce snippets "<paste the content of the template between quotes>"
-
-3. Create customized information reporting snippets using the stored template.
-   Example: GPT-Prompt> Create a snippet with comments for use with the template that reports specific information.
-
-4. Add requirements, error control, and further enhancements to the snippets.
-   Example: GPT-Prompt> If the snippet requires a module, check if it is available and import it if not.
-
-5. Enhance the snippets with appropriate error control.
-   Example: GPT-Prompt> Use "SilentlyContinue" and only report errors in the catch block.
-
-6. Get an example of the output for a better understanding.
-   Example: GPT-Prompt> Show me an example of the report output.
-
+   1. Copy the provided template from the repository.
+   2. Utilize ChatGPT to interact with the template and store it for later use.
+       Example: GPT-Prompt> Store the following as a template to produce snippets "<paste the content of the template between quotes>"
+   3. Create customized information reporting snippets using the stored template.
+       Example: GPT-Prompt> Create a snippet with comments for use with the template that reports specific information.
+   4. Add requirements, error control, and further enhancements to the snippets.
+       Example: GPT-Prompt> If the snippet requires a module, check if it is available and import it if not.
+   5. Enhance the snippets with appropriate error control.
+       Example: GPT-Prompt> Use "SilentlyContinue" and only report errors in the catch block.
+   6. Get an example of the output for a better understanding.
+       Example: GPT-Prompt> Show me an example of the report output.
 .PARAMETER None
     This script does not require any parameters to be passed.
-
 .EXAMPLE !!! Set-Execution Policy to Bypass !!!
     Set-ExecutionPolicy -Scope Process Bypass -Force
     .\PoShDiscoveryReport.ps1
     Runs the script and generates a txt report file on the desktop with details collected.
-
 .NOTES
-Make sure to customize the snippets based on your specific environment and requirements.
-
+    Make sure to customize the snippets based on your specific environment and requirements.
 .NOTES
     Version: 1.0
     Creation Date: 2023-05-09
